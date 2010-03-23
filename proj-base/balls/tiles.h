83da4d9dbaee614d408a3846e7b3a02db405144a
David M. Koppelman <koppel@ece.lsu.edu>
2010-03-23 19:09:00 +0000

    tile_ball_collide:  Handle corner cases (literally).  Code by John Harvey.

--- proj-base/balls/tiles.h
+++ proj-base/balls/tiles.h
@@ -163,15 +163,28 @@ tile_ball_collide
 (Tile *tile, Ball *ball, pCoor& pball_pos, float opt_ball_radius)
 {
   pVect tile_to_ball(tile->pt_ll,ball->position);
-  const float dist = dot(tile_to_ball,tile->normal);
+
+  // Distance from tile's plane to the ball.
+  const float dist = dot(tile_to_ball,tile->normal); 
+
   if ( fabs(dist) > opt_ball_radius ) return false;
-  pCoor pt_closest = ball->position - dist * tile->normal;
-  const float dist_ht = dot(tile->norm_up,tile_to_ball);
-  if ( dist_ht < -opt_ball_radius ) return false;
+
+  // The closest point on tile plane to the ball.
+  pCoor pt_closest = ball->position - dist * tile->normal; 
+
+  // How far up the tile in the y direction the center of the ball sits
+  const float dist_ht = dot(tile->norm_up,tile_to_ball);  
+
+  if ( dist_ht < -opt_ball_radius ) return false; 
   if ( dist_ht > tile->height + opt_ball_radius ) return false;
+
+  // How far up the tile in the x direction the center of the ball sits
   const float dist_wd = dot(tile->norm_rt,tile_to_ball);
   if ( dist_wd < -opt_ball_radius ) return false;
   if ( dist_wd > tile->width + opt_ball_radius ) return false;
+
+  // If ball touching tile surface (not including an edge or corner)
+  // then set up the pseudo ball for collision handling
   if ( dist_ht >= 0 && dist_ht <= tile->height
        && dist_wd >= 0 && dist_wd <= tile->width )
     {
@@ -181,6 +194,42 @@ tile_ball_collide
       return true;
     }
 
+  // Test whether the ball is touching a corner
+  if ( ( dist_ht < 0 || dist_ht > tile->height ) 
+       && ( dist_wd < 0 || dist_wd > tile->width) )
+    {
+      pCoor ref_pt;
+
+      // We need to place the pseudo ball based upon the vector from
+      // ball position to the corner. First step is to figure out which
+      // corner.
+
+      if ( dist_ht < 0 && dist_wd < 0 ) 
+        {
+          ref_pt = tile->pt_ll;
+        }
+      else if ( dist_ht < 0 && dist_wd > tile->width ) 
+        {
+          ref_pt = tile->pt_lr;
+        }
+      else if ( dist_ht > tile->height && dist_wd < 0 ) 
+        {
+          ref_pt = tile->pt_ul;
+        }
+      else 
+        {
+          ref_pt = tile->pt_ll+tile->vec_rt+tile->vec_up;
+        }
+
+      pNorm ball_to_corner(ball->position,ref_pt);
+      tile->ball_tested = ball;
+      pball_pos = ref_pt + opt_ball_radius * ball_to_corner;
+
+      return true;
+    }
+
+  // Else the ball is touching an edge
+
   const bool tact_horiz = dist_ht < 0 || dist_ht > tile->height;
   const pVect corner_to_tact =
     tact_horiz ? dist_wd * tile->norm_rt : dist_ht * tile->norm_up;
@@ -188,6 +237,9 @@ tile_ball_collide
     tact_horiz ? ( dist_ht < 0 ? tile->pt_ll : tile->pt_ul ) :
     ( dist_wd < 0 ? tile->pt_ll : tile->pt_lr );
 
+  tile->ball_tested = ball;
+
+  // Find the closest edge point of the tile to the ball
   pCoor tact = ref_pt + corner_to_tact;
   pNorm ball_to_tact(ball->position,tact);
   pball_pos = tact + opt_ball_radius * ball_to_tact;
