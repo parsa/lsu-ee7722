ef3a1111e581300c61bd1ba19f138de98cda89da
David M. Koppelman <koppel@ece.lsu.edu>
2016-11-03 16:13:27 -0500

    Put in util-container and new coord-container headers.

---
+++ include/coord-containers.h
@@ -0,0 +1,15 @@
+// -*- c++ -*-
+
+#ifndef COORD_CONTAINERS_H
+#define COORD_CONTAINERS_H
+
+#include "util-containers.h"
+#include "coord.h"
+
+typedef pVector<pVect> pVects;
+typedef pVector<pCoor> pCoors;
+typedef pVector<pColor> pColors;
+typedef pVector<pMatrix3x3> pMatrix3x3s;
+typedef pVector<pMatrix> pMatrixs;
+
+#endif
