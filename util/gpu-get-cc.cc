44589a05ef62390629d902ac8629c66c1e8f7e8c
David M. Koppelman <koppel@ece.lsu.edu>
2017-09-29 15:16:03 -0500

    Avoid compile errors when CUDA version changes by hashing .d file names.

--- util/gpu-get-cc.cc
+++ util/gpu-get-cc.cc
@@ -20,11 +20,19 @@ main(int argc, char **argv)
       return 0;
     }
 
+  const char* const path = argv[1];
+
+  if ( string(path) == "-dhash" )
+    {
+      printf("c%d-cc%d%d.d",
+             info.cuda_version, info.cc_major, info.cc_minor);
+      return 0;
+    }
+
   pStringF correct_contents
     ("Compiled for CUDA CC %d.%d  CUDA Version %d",
      info.cc_major,info.cc_minor,info.cuda_version);
 
-  const char* const path = argv[1];
   ifstream curr_file(path);
   bool update_needed = false;
   if ( curr_file.is_open() )
