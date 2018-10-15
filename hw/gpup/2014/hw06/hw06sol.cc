376fb749e681af5ce1972c782c84f790ed52af52
David M. Koppelman <koppel@ece.lsu.edu>
2018-10-15 10:42:53 -0500

    2014, 2018/hw03: remove unneeded header files.

--- hw/gpup/2014/hw06/hw06sol.cc
+++ hw/gpup/2014/hw06/hw06sol.cc
@@ -65,17 +65,11 @@
 
 
 #define GL_GLEXT_PROTOTYPES
-#define GLX_GLXEXT_PROTOTYPES
 
 #include <GL/gl.h>
-#include <GL/glext.h>
-#include <GL/glx.h>
-#include <GL/glxext.h>
-#include <GL/glu.h>
 #include <GL/freeglut.h>
 
 #include <gp/util.h>
-#include <gp/glextfuncs.h>
 #include <gp/coord.h>
 #include <gp/shader.h>
 #include <gp/pstring.h>
