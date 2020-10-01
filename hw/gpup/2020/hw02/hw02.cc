e99ca3f110eb8915f1c9612f7d72b50dda4b728e
David M. Koppelman <koppel@ece.lsu.edu>
2020-10-01 16:07:44 -0500

    hw02: A few more pre-solution changes.

--- hw/gpup/2020/hw02/hw02.cc
+++ hw/gpup/2020/hw02/hw02.cc
@@ -24,6 +24,7 @@
  //   Will move object or push ball, depending on mode:
  //   'e': Move eye.
  //   'l': Move light.
+ //   's': Move construction markers. (Show eta, theta)
  //
  /// Eye Direction
  //   Home, End, Delete, Insert
@@ -89,11 +90,13 @@ public:
   //
   pFrame_Timer frame_timer;
 
+  bool opt_tryout1, opt_tryout2;
+
   bool opt_show_lines; // Set normal to triangle normal.
 
   pCoor light_location;
   float opt_light_intensity;
-  enum { MI_Eye, MI_Light, MI_Ball, MI_Ball_V, MI_COUNT } opt_move_item;
+  enum { MI_Eye, MI_Light, MI_Ball, MI_Slice, MI_COUNT } opt_move_item;
 
   int slices; // Level of detail for sphere.
   pCoor sphere_location;
@@ -102,6 +105,13 @@ public:
   pCoor eye_location;
   pVect eye_direction;
   pMatrix modelview;
+
+  /// Homework 2
+
+  bool opt_show;
+  float show_delta_eta_seen, show_delta_theta_seen;
+  int opt_change_theta, opt_change_eta;
+  float opt_show_theta, opt_show_eta;
 
 };
 
@@ -128,6 +138,15 @@ World::init()
   opt_move_item = MI_Eye;
 
   opt_show_lines = false;
+
+  opt_show = true;
+  opt_change_eta = 0;
+  opt_change_theta = 0;
+  opt_show_theta = 0.8;
+  show_delta_theta_seen = -1;
+  opt_show_eta = 1.73;
+  show_delta_eta_seen = -1;
+
 }
 
 void
@@ -192,6 +211,13 @@ World::render()
   ogl_helper.fbprintf
     ("Light location: [%5.1f, %5.1f, %5.1f]\n",
      light_location.x, light_location.y, light_location.z);
+
+  ogl_helper.fbprintf
+    ("HW02: Show slice: %3s ('c' to toggle)  "
+     "Show eta: %5.3f,  theta: %5.3f (%sarrows to change)\n",
+     opt_show ? "ON " : "OFF",
+     opt_show_eta, opt_show_theta,
+     opt_move_item != MI_Slice ? "'s' then " : "");
 
   pVariable_Control_Elt* const cvar = variable_control.current;
   ogl_helper.fbprintf("VAR %s = %.5f  (TAB or '`' to change, +/- to adjust)\n",
@@ -278,6 +304,7 @@ World::render()
   //
   const pColor lsu_spirit_purple(0x580da6);
   const pColor lsu_spirit_gold(0xf9b237);
+  const pColor color_red(0xaa0000);
 
   /// Start Sending Triangles
   //
@@ -285,14 +312,23 @@ World::render()
 
   glBegin(opt_show_lines ? GL_LINE_STRIP : GL_TRIANGLES);
 
-  // Set current color.  This is carried with vertex.
-  //
-  glColor3fv(lsu_spirit_gold);
 
   const float delta_eta = M_PI / slices;
+  const int slice_max = slices - 2;
 
-  // Outer (eta) Loop: Iterate over longitude (north-to-south).
-  // Inner (theta) Loop: Iterate over latitude (east-to-west)
+  if ( opt_change_eta || delta_eta != show_delta_eta_seen )
+    {
+      const int prev_slice = opt_show_eta / delta_eta + 0.5;
+      const int targ_slice = prev_slice + opt_change_eta;
+      const int slice =
+        targ_slice < 0 ? slice_max : targ_slice > slice_max ? 0 : targ_slice;
+      opt_show_eta = slice * delta_eta;
+      opt_change_eta = 0;
+      show_delta_eta_seen = delta_eta;
+    }
+
+  // Outer (slice,eta)  Loop: Iterate over longitude (north-to-south).
+  // Inner (segs,theta) Loop: Iterate over latitude (east-to-west)
 
   for ( int slice = 0; slice < slices - 1; slice++ )
     {
@@ -300,11 +336,28 @@ World::render()
       const float eta1 = eta0 + delta_eta;
       const float y0 = cosf(eta0),        y1 = cosf(eta1);
       const float slice_r0 = sinf(eta0),  slice_r1 = sinf(eta1);
-      const float delta_theta = delta_eta * slice_r1;
+      const int segs = max(3, int( 2 * slices * max(slice_r0,slice_r1) ) );
+      const float delta_theta = 2 * M_PI / segs;
+      const bool show_eta = eta0 == opt_show_eta;
+      pColor slice_color = show_eta ? lsu_spirit_purple : lsu_spirit_gold;
 
-      for ( float theta0 = 0; theta0 < 2 * M_PI; theta0 += delta_theta )
+      if ( show_eta &&
+           ( opt_change_theta || delta_theta != show_delta_theta_seen ) )
         {
+          const int prev_slice = opt_show_theta / delta_theta + 0.5;
+          const int seg = ( segs + prev_slice + opt_change_theta ) % segs;
+          opt_show_theta = seg * delta_theta;
+          opt_change_theta = 0;
+          show_delta_theta_seen = delta_theta;
+        }
+
+      for ( int seg = 0; seg < segs; seg++ )
+        {
+          const float theta0 = seg * delta_theta;
           const float theta1 = theta0 + delta_theta;
+
+          const bool show_theta = show_eta && theta0 == opt_show_theta;
+          glColor3fv( show_theta ? color_red : slice_color );
 
           /// Triangle 1
 
@@ -375,9 +428,11 @@ World::cb_keyboard()
   case FB_KEY_INSERT: user_rot_axis.y =  -1; break;
   case FB_KEY_HOME: user_rot_axis.x = 1; break;
   case FB_KEY_END: user_rot_axis.x = -1; break;
+  case 'c': case 'C': opt_show = !opt_show; break;
   case 'b': case 'B': opt_move_item = MI_Ball; break;
   case 'e': case 'E': opt_move_item = MI_Eye; break;
   case 'l': case 'L': opt_move_item = MI_Light; break;
+  case 's': case 'S': opt_move_item = MI_Slice; break;
   case 'n': opt_show_lines = !opt_show_lines; break;
   case 'y': opt_tryout1 = !opt_tryout1; break;
   case 'Y': opt_tryout2 = !opt_tryout2; break;
@@ -405,13 +460,19 @@ World::cb_keyboard()
         fabs(eye_direction.y) > 0.99
         ? 0 : atan2(eye_direction.x,-eye_direction.z);
       pMatrix_Rotation rotall(pVect(0,1,0),-angle);
-      adjustment *= rotall;
+      pVect radjustment = rotall * adjustment;
 
       switch ( opt_move_item ){
-      case MI_Light: light_location += adjustment; break;
-      case MI_Eye: eye_location += adjustment; break;
-      case MI_Ball: sphere_location += adjustment; break;
-      default: break;
+      case MI_Light: light_location += radjustment; break;
+      case MI_Eye: eye_location += radjustment; break;
+      case MI_Ball: sphere_location += radjustment; break;
+      case MI_Slice:
+        if ( adjustment.x )
+          opt_change_theta = adjustment.x < 0 ? -1 : 1;
+        else if ( const float adj = -adjustment.y + adjustment.z )
+          opt_change_eta = adj < 0 ? -1 : 1;
+        break;
+      default: assert( false );
       }
     }
 }
